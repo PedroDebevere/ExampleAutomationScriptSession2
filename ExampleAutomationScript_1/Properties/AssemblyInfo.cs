3ece5fa7d6aa0616e778e7038baa7d449eee8d7d
Pedro Debevere <pedro.debevere@skyline.be>
2023-03-28 11:36:54 +0200

    Add project files.

---
+++ ExampleAutomationScript_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExampleAutomationScript_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("ExampleAutomationScript_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("40FAFEA9-C9E8-4D56-95AD-F7CE63990F07")]
+[assembly: AssemblyVersion("1.0.0.0")]
